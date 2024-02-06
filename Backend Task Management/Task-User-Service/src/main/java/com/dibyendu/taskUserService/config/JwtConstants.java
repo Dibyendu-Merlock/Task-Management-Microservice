e0fe4aa808f1a9e8f34ec51391015c51dd4fd897
Dibyendu-Merlock <107022870+Dibyendu-Merlock@users.noreply.github.com>
2024-02-07 00:40:38 +0530

    All Done
    All done

--- Backend Task Management/Task-User-Service/src/main/java/com/dibyendu/taskUserService/config/JwtConstants.java
+++
@@ -1,7 +0,0 @@
-package com.dibyendu.taskUserService.config;
-
-public class JwtConstants
-{
-    public static final String SECRET_KEY = "snfknSNKDfihsndfbhsibdfkbnsigdfibnswjfbisgdfyugisbdf";
-    public static final String JWT_HEADER = "Authorization";
-}
